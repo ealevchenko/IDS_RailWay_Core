66083fa754b7f28ab21cfd42dcab9e7c4af2f08a
ealevchenko <eduard.levchenko@arcelormittal.com>
2024-02-09 16:58:07 +0200

    IDSRW_UI Добавил вариант стартовой страницы

---
+++ IDSRW_UI/Areas/Report/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IDSRW_UI.Areas.Report.Controllers
+{
+    [Area("Report")]
+    public class HomeController : Controller
+    {
+        [Route("{area}")]
+        [Route("{area}/{controller}")]
+        [Route("{area}/{controller}/{action}")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
