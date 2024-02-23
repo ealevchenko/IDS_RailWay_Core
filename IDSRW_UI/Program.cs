10a6ae481c835ce73ec418a291ff81b2b1451dec
ealevchenko <eduard.levchenko@arcelormittal.com>
2024-02-23 12:13:57 +0200

    маршрут

--- IDSRW_UI/Program.cs
+++ IDSRW_UI/Program.cs
@@ -23,13 +23,11 @@ app.UseAuthorization();
 app.UseEndpoints(endpoints =>
 {
     endpoints.MapControllerRoute(
-      name: "default",
-      pattern: "/{controller=Home}/{action=Index}/{id?}");
-    endpoints.MapControllerRoute(
       name: "areas",
-      pattern: "/{area:exists}/{controller=Home}/{action=Index}/{id?}"
-    );
-
+      pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+    endpoints.MapControllerRoute(
+      name: "default",
+      pattern: "{controller=Home}/{action=Index}/{id?}");
 });
 //app.MapControllerRoute(
 //    name: "default",
