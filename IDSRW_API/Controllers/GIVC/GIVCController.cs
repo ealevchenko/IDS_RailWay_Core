f548cdb5af20d275fc004b15be6e191ffba1ed58
ealevchenko <eduard.levchenko@arcelormittal.com>
2024-03-01 09:25:16 +0200

    idsrw_api  убрал запрос к ГИВЦ  Добавил параметр кол строк по выпбору списка запросов

--- IDSRW_API/Controllers/GIVC/GIVCController.cs
+++ IDSRW_API/Controllers/GIVC/GIVCController.cs
@@ -34,14 +34,26 @@ namespace WebAPI.Controllers.GIVC
         {
             return await repo.RetrieveAllAsync();
         }
-        // GET: Givc/type_requests
-        [HttpGet("Request/type_requests/{type_requests}")]
-        public async Task<IActionResult> GetListGivcRequestOfType(string type_requests)
+        // GET: Givc/Request/type_requests/req1892/count/10
+        [HttpGet("Request/type_requests/{type_requests}/count/{count:int}")]
+        public async Task<IActionResult> GetListGivcRequestOfType(string type_requests, int? count)
         {
             IEnumerable<GivcRequest> list = await repo.RetrieveAllAsync();
-            if (list == null) return NotFound();
-            var res = list.Where(c => c.TypeRequests == type_requests).OrderByDescending(c => c.DtRequests).Select(u => new { u.Id, u.DtRequests, u.TypeRequests,u.ParametersReguest, u.CountLine, u.Create, u.CreateUser });
-            return res != null ? new ObjectResult(res) : NotFound();
+            string mes_not_data = "По указанному типу запроса нет данных!";
+            string mes_not_type_requests = "Не указан тип справки!";
+            if (String.IsNullOrWhiteSpace(type_requests)) return BadRequest(new { message = mes_not_type_requests });
+            if (list == null) return BadRequest(new { message = mes_not_data });
+            if (count > 0)
+            {
+                list = list.Where(c => c.TypeRequests == type_requests).OrderByDescending(c => c.Id).Take((int)count);
+            }
+            else
+            {
+                list = list.Where(c => c.TypeRequests == type_requests).OrderByDescending(c => c.Id);
+            }
+
+            var res = list.Select(u => new { u.Id, u.DtRequests, u.TypeRequests, u.ParametersReguest, u.CountLine, u.Create, u.CreateUser });
+            return res != null ? new ObjectResult(res) : BadRequest(new { message = mes_not_data });
         }
 
         // GET: Givc/Request/[id]
@@ -93,38 +105,39 @@ namespace WebAPI.Controllers.GIVC
             GivcRequest added = await repo.CreateAsync(c);
             return CreatedAtRoute("GetGivcRequest", new { id = added.Id }, c); // 201 Created
         }
+        ///TODO! Блаканул бабло
         // POST: Givc/Request
         // BODY: GivcRequest (JSON, XML)
-        [HttpPost]
-        public async Task<IActionResult> Request([FromBody] parameters_reguest parameters)
-        {
-            IDS_GIVC ids_givc = new IDS_GIVC(_logger, _configuration);
-            // Текущая дата
-            DateTime dt_curr = DateTime.Now;
-            DateTime cur_date = dt_curr.Date;
-            int cur_day = dt_curr.Date.Day;
-            int cur_hour = dt_curr.Hour;
-            // Получим последний запрос
-            GivcRequest? last_givc_req = ids_givc.GetLastGivcRequest(parameters.type_requests);
-            DateTime? dt_last = last_givc_req != null ? last_givc_req.DtRequests : null;
-            string s_param = System.Text.Json.JsonSerializer.Serialize(parameters); // Сериализуем parameters
-            if (dt_last != null)
-            {
-                int last_day = ((DateTime)dt_last).Date.Day;
-                int last_hour = ((DateTime)dt_last).Hour;
-                // Проверка на исключение повторного запроса
-                if (s_param == last_givc_req.ParametersReguest && cur_day == last_day && cur_hour == last_hour)
-                { return BadRequest(new { message = "The request has already been executed!" }); } // 400 Bad request
+        //[HttpPost]
+        //public async Task<IActionResult> Request([FromBody] parameters_reguest parameters)
+        //{
+        //    IDS_GIVC ids_givc = new IDS_GIVC(_logger, _configuration);
+        //    // Текущая дата
+        //    DateTime dt_curr = DateTime.Now;
+        //    DateTime cur_date = dt_curr.Date;
+        //    int cur_day = dt_curr.Date.Day;
+        //    int cur_hour = dt_curr.Hour;
+        //    // Получим последний запрос
+        //    GivcRequest? last_givc_req = ids_givc.GetLastGivcRequest(parameters.type_requests);
+        //    DateTime? dt_last = last_givc_req != null ? last_givc_req.DtRequests : null;
+        //    string s_param = System.Text.Json.JsonSerializer.Serialize(parameters); // Сериализуем parameters
+        //    if (dt_last != null)
+        //    {
+        //        int last_day = ((DateTime)dt_last).Date.Day;
+        //        int last_hour = ((DateTime)dt_last).Hour;
+        //        // Проверка на исключение повторного запроса
+        //        if (s_param == last_givc_req.ParametersReguest && cur_day == last_day && cur_hour == last_hour)
+        //        { return BadRequest(new { message = "The request has already been executed!" }); } // 400 Bad request
 
-            }
-            GivcRequest? req = ids_givc.RequestToGIVC(parameters, null); ;
-            if (req == null)
-            {
-                return BadRequest(); // 400 Bad request
-            }
-            GivcRequest added = await repo.CreateAsync(req);
-            return CreatedAtRoute("GetGivcRequest", new { id = added.Id }, req); // 201 Created
-        }
+        //    }
+        //    GivcRequest? req = ids_givc.RequestToGIVC(parameters, null); ;
+        //    if (req == null)
+        //    {
+        //        return BadRequest(); // 400 Bad request
+        //    }
+        //    GivcRequest added = await repo.CreateAsync(req);
+        //    return CreatedAtRoute("GetGivcRequest", new { id = added.Id }, req); // 201 Created
+        //}
 
 
         //// PUT: api/GivcRequest/[id]
