81b56383a6fc0cfe60f8005e1e29e1ca5d35c6ce
ealevchenko <eduard.levchenko@arcelormittal.com>
2024-03-01 10:35:13 +0200

    IDSRW_UI - add Log

--- IDSRW_UI/Program.cs
+++ IDSRW_UI/Program.cs
@@ -1,61 +1,83 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.FileSystemGlobbing.Internal;
 using System.Xml.Linq;
+using NLog;
+using NLog.Web;
 
 //System.Environment.CurrentDirectory = System.AppDomain.CurrentDomain.BaseDirectory;
+var logger = NLog.LogManager.Setup().LoadConfigurationFromAppSettings().GetCurrentClassLogger();
+logger.Debug("init main");
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-builder.Services.AddControllersWithViews();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+try
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    var builder = WebApplication.CreateBuilder(args);
+
+    // Add services to the container.
+    builder.Services.AddControllersWithViews();
+
+    // NLog: Setup NLog for Dependency injection
+    builder.Logging.ClearProviders();
+    builder.Host.UseNLog();
+
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    if (!app.Environment.IsDevelopment())
+    {
+        app.UseExceptionHandler("/Home/Error");
+        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        app.UseHsts();
+    }
+
+    app.UseHttpsRedirection();
+    app.UseStaticFiles();
+    app.UseRouting();
+
+    app.UseEndpoints(endpoints =>
+    {
+        endpoints.MapControllerRoute(
+            name: "MyArea",
+          pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+        endpoints.MapControllerRoute(
+            name: "default",
+            pattern: "{controller=Home}/{action=Index}/{id?}");
+    });
+
+
+    //app.UseAuthorization();
+
+    //app.MapControllerRoute(
+    //    name: "default",
+    //    pattern: "{controller=Home}/{action=Index}/{id?}");
+    //app.MapAreaControllerRoute(
+    //    name: "areas",
+    //    areaName : "default",
+    //    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+    //    );
+
+    //app.UseRouting();
+    //app.UseEndpoints(endpoints =>
+    //{
+    //    endpoints.MapControllerRoute(
+    //      name: "areas",
+    //      pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+    //    endpoints.MapControllerRoute(
+    //      name: "default",
+    //      pattern: "{controller=Home}/{action=Index}/{id?}");
+    //});
+    //app.MapControllerRoute(
+    //    name: "default",
+    //    pattern: "{controller=Home}/{action=Index}/{id?}");
+    app.Run();
 }
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-app.UseRouting();
-
-app.UseEndpoints(endpoints =>
+catch (Exception exception)
 {
-    endpoints.MapControllerRoute(
-        name: "MyArea",
-      pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
-    endpoints.MapControllerRoute(
-        name: "default",
-        pattern: "{controller=Home}/{action=Index}/{id?}");
-});
-
-
-//app.UseAuthorization();
-
-//app.MapControllerRoute(
-//    name: "default",
-//    pattern: "{controller=Home}/{action=Index}/{id?}");
-//app.MapAreaControllerRoute(
-//    name: "areas",
-//    areaName : "default",
-//    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
-//    );
-
-//app.UseRouting();
-//app.UseEndpoints(endpoints =>
-//{
-//    endpoints.MapControllerRoute(
-//      name: "areas",
-//      pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
-//    endpoints.MapControllerRoute(
-//      name: "default",
-//      pattern: "{controller=Home}/{action=Index}/{id?}");
-//});
-//app.MapControllerRoute(
-//    name: "default",
-//    pattern: "{controller=Home}/{action=Index}/{id?}");
-app.Run();
+    // NLog: catch setup errors
+    logger.Error(exception, "Stopped program because of exception");
+    throw;
+}
+finally
+{
+    // Ensure to flush and stop internal timers/threads before application-exit (Avoid segmentation fault on Linux)
+    NLog.LogManager.Shutdown();
+}
