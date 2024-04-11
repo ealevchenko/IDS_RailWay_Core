605488b8b00ba88718b548ac16f64d835e22b14b
ealevchenko <eduard.levchenko@arcelormittal.com>
2024-04-11 13:54:50 +0300

    IDSRW_API add controller WSDController

---
+++ IDSRW_API/Controllers/WS/WSDController.cs
@@ -0,0 +1,46 @@
+﻿using EF_IDS.Concrete;
+using EF_IDS.Entities;
+using EF_IDS.Functions;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using WebAPI.Repositories;
+using WebAPI.Repositories.Directory;
+using EFIDS.Functions;
+
+namespace WebAPI.Controllers.Directory
+{
+
+    [Route("[controller]")]
+    [ApiController]
+    public class WSDController : ControllerBase
+    {
+        private EFDbContext db;
+
+        public WSDController(EFDbContext db)
+        {
+            this.db = db;
+        }
+
+        // GET: WSD/view/wagon/way/115
+        [HttpGet("view/wagon/way/{id_way}")]
+        public async Task<ActionResult<IEnumerable<ViewCarWay>>> GetViewWagonsOfIdWay(int id_way)
+        {
+            try
+            {
+                List<ViewCarWay> result = await db.getViewWagonsOfIdWay(id_way).ToListAsync();
+                if (result == null)
+                    return NotFound();
+                return Ok(result);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+    }
+}
