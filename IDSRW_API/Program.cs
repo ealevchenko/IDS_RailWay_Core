24c2247c77249133ae209fcb57c5fd2e20f1be09
ealevchenko <eduard.levchenko@arcelormittal.com>
2024-04-23 16:21:34 +0300

    AddAuthentication

--- IDSRW_API/Program.cs
+++ IDSRW_API/Program.cs
@@ -42,6 +42,14 @@ try
 
     //builder.Services.AddScoped<EF_IDS.Abstract.IRepository<GivcRequest>, EFGivcRequest>();
 
+    builder.Services.AddAuthentication(NegotiateDefaults.AuthenticationScheme)
+   .AddNegotiate();
+
+    builder.Services.AddAuthorization(options =>
+    {
+        // By default, all incoming requests will be authorized according to the default policy.
+        options.FallbackPolicy = options.DefaultPolicy;
+    });
 
     //builder.Services.AddAuthentication(NegotiateDefaults.AuthenticationScheme)
     //   .AddNegotiate();
