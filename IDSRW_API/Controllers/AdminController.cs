c1100b1fe6ef3aaa0835ab37f41ba2d43de4f0a7
ealevchenko <eduard.levchenko@arcelormittal.com>
2024-04-23 16:22:13 +0300

    add AdminController

---
+++ IDSRW_API/Controllers/AdminController.cs
@@ -0,0 +1,34 @@
+﻿using EF_IDS.Functions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IDSRW_API.Controllers
+{
+    public class UserInfo
+    {
+        public string Name { get; set; }
+        public bool IsAuthenticated { get; set; }
+    }
+
+    [Route("[controller]")]
+    [ApiController]
+    public class AdminController : ControllerBase
+    {
+        public AdminController() { }
+
+        // GET: Admin/user_info
+        [HttpGet("user_info")]
+        public async Task<ActionResult<UserInfo>> GetUserInfo()
+        {
+            try
+            {
+                UserInfo result = new UserInfo() { Name = HttpContext.User.Identity.Name, IsAuthenticated = HttpContext.User.Identity.IsAuthenticated };
+                return new ObjectResult(result);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+    }
+}
