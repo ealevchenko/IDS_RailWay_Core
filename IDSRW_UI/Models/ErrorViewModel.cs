66829c9ad1d4ccc139d0494529a6d1349e81cb74
ealevchenko <eduard.levchenko@arcelormittal.com>
2024-05-13 15:55:36 +0300

    delete IDSRW_UI

--- IDSRW_UI/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace WebCore_UI.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
