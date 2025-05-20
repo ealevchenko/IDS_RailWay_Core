5def505a0d6ad0d5f27db0c8cc07ba2fff7b5f39
Levchenko <eduard.levchenko@arcelormittal.com>
2025-05-20 11:33:48 +0300

    Сверка по отправке обновление сверки +убрал фиксацию сверки если сбрасывали пустой перечень

--- IDSRW_API/Controllers/Outgoing/OutgoingUzDocumentController.cs
+++ IDSRW_API/Controllers/Outgoing/OutgoingUzDocumentController.cs
@@ -30,6 +30,13 @@ namespace WebAPI.Controllers.Directory
         public long? value { get; set; }
     }
 
+    public class UpdateVerificationOutgoing
+    {
+        public List<long> id_docs { get; set; }
+        public int? num_list { get; set; }
+        public DateTime? date_list { get; set; }
+    }
+
     #endregion
 
     [Route("[controller]")]
@@ -469,159 +476,49 @@ namespace WebAPI.Controllers.Directory
             }
         }
 
-        //#region СВЕРКА ДОКУМЕНТОВ ПО ПРИБЫТИЮ
-        ///// <summary>
-        ///// Сверака документов по прибытию получение документа по id 
-        ///// </summary>
-        ///// <param name="id"></param>
-        ///// <returns></returns>
-        //// GET: OutgoingUzDocument/verification/id/932003
-        //[HttpGet("verification/id/{id}")]
-        //public async Task<ActionResult<OutgoingUzDocument>> GetVerificationOutgoingUzDocument(long id)
-        //{
-        //    try
-        //    {
-        //        var result = await db.OutgoingUzDocuments
-        //                .AsNoTracking()
-        //                .Where(x => x.Id == id)
-        //                .Include(code_bc => code_bc.CodeBorderCheckpointNavigation)
-        //                .Include(code_st_from => code_st_from.CodeStnFromNavigation)
-        //                .Include(code_st_on => code_st_on.CodeStnToNavigation)
-        //                .Include(code_cns => code_cns.CodeConsigneeNavigation)
-        //                .Include(code_chp => code_chp.CodeShipperNavigation)
-        //                .Include(code_ps => code_ps.CodePayerSenderNavigation)
-        //                .Include(code_pa => code_pa.CodePayerArrivalNavigation)
-        //                .Include(code_pl => code_pl.CodePayerLocalNavigation)
-        //                .Include(doc => doc.OutgoingUzDocumentDocs)
-        //                .Include(act => act.OutgoingUzDocumentActs)
-        //                .Include(pays => pays.OutgoingUzDocumentPays)
-        //                .Include(wag_doc => wag_doc.OutgoingUzVagons)
-        //                    .ThenInclude(out_sost => out_sost.IdArrivalNavigation)
-        //                .Include(wag_doc => wag_doc.OutgoingUzVagons)
-        //                    .ThenInclude(wag_acts => wag_acts.OutgoingUzVagonActs)
-        //                .Include(wag_doc => wag_doc.OutgoingUzVagons)
-        //                    .ThenInclude(wag_cont => wag_cont.OutgoingUzVagonConts)
-        //                        .ThenInclude(wag_cont_pays => wag_cont_pays.OutgoingUzContPays)
-        //                .Include(wag_doc => wag_doc.OutgoingUzVagons)
-        //                    .ThenInclude(wag_pays => wag_pays.OutgoingUzVagonPays)
-        //                .Include(wag_doc => wag_doc.OutgoingUzVagons)
-        //                    .ThenInclude(wag_cargo => wag_cargo.IdCargoNavigation)
-        //                .Include(wag_doc => wag_doc.OutgoingUzVagons)
-        //                    .ThenInclude(wag_rent => wag_rent.IdWagonsRentArrivalNavigation)
-        //                        .ThenInclude(wag_oper => wag_oper.IdOperatorNavigation)
-        //                .Include(wag_doc => wag_doc.OutgoingUzVagons)
-        //                    .ThenInclude(wag_div => wag_div.IdDivisionOnAmkrNavigation)
-        //                .FirstOrDefaultAsync();
+        #region СВЕРКА ДОКУМЕНТОВ ПО ПРИБЫТИЮ
+        // POST: OutgoingUzDocument/update/verification
+        // BODY: OutgoingUzDocument/update/verification (JSON, XML)
+        [HttpPost("update/verification")]
+        public async Task<int> PostVerificationOutgoingUzDocument([FromBody] UpdateVerificationOutgoing value)
+        {
+            try
+            {
+                string user = HttpContext.User.Identity.Name;
+                bool IsAuthenticated = HttpContext.User.Identity.IsAuthenticated;
+                // Пользователь непрошел интендификацию!
+                if (value == null || !IsAuthenticated) { return -1; }
+                //if (value.presented < 1 || value.presented > 3) return (int)errors_base.error_input_value;
+                List<OutgoingUzDocument>? list = await db.OutgoingUzDocuments
+                    .Where(d => value.id_docs.Contains(d.Id))
+                    .ToListAsync();
+                if (list != null)
+                {
+                    foreach (OutgoingUzDocument doc in list)
+                    {
+                        doc.NumList = value.num_list;
+                        doc.DateList = value.date_list;
+                        // если сбоасываем пустую строку, тогда не делаем отметку
+                        if ((value.num_list == null && doc.Verification != null) || value.num_list != null)
+                        {
+                            doc.Verification = DateTime.Now;
+                            doc.VerificationUser = user;
+                        }
 
-        //        if (result == null)
-        //            return NotFound();
-        //        return new ObjectResult(result);
-        //    }
-        //    catch (Exception e)
-        //    {
-        //        return BadRequest(e.Message);
-        //    }
-        //}
-        ///// <summary>
-        ///// Сверака документов по прибытию получение документа за период 
-        ///// </summary>
-        ///// <param name="start"></param>
-        ///// <param name="stop"></param>
-        ///// <returns></returns>
-        //// GET: OutgoingUzDocument/verification/start/2025-03-01T00:00:00/stop/2025-03-30T00:00:00
-        //[HttpGet("verification/start/{start:DateTime}/stop/{stop:DateTime}")]
-        //public async Task<ActionResult<OutgoingUzDocument>> GetVerificationOutgoingUzDocument(DateTime start, DateTime stop)
-        //{
-        //    try
-        //    {
-        //        IEnumerable<long> id_sts = db.OutgoingSostavs.AsNoTracking().Where(s => s.DateAdoption >= start && s.DateAdoption <= stop).Select(c => c.Id).ToList();
-        //        IEnumerable<long> id_docs = db.OutgoingUzVagons.Where(v => id_sts.Contains(v.IdArrivalNavigation.Id)).Select(c => c.IdDocument).Distinct().ToList();
-        //        var result = await db.OutgoingUzDocuments
-        //                .AsNoTracking()
-        //                .Where(x => id_docs.Contains(x.Id) && x.CalcPayer!=null)
-        //                .Include(code_bc => code_bc.CodeBorderCheckpointNavigation)
-        //                .Include(code_st_from => code_st_from.CodeStnFromNavigation)
-        //                .Include(code_st_on => code_st_on.CodeStnToNavigation)
-        //                .Include(code_cns => code_cns.CodeConsigneeNavigation)
-        //                .Include(code_chp => code_chp.CodeShipperNavigation)
-        //                .Include(code_ps => code_ps.CodePayerSenderNavigation)
-        //                .Include(code_pa => code_pa.CodePayerArrivalNavigation)
-        //                .Include(code_pl => code_pl.CodePayerLocalNavigation)
-        //                .Include(doc => doc.OutgoingUzDocumentDocs)
-        //                .Include(act => act.OutgoingUzDocumentActs)
-        //                .Include(pays => pays.OutgoingUzDocumentPays)
-        //                .Include(wag_doc => wag_doc.OutgoingUzVagons)
-        //                    .ThenInclude(out_sost => out_sost.IdArrivalNavigation)
-        //                .Include(wag_doc => wag_doc.OutgoingUzVagons)
-        //                    .ThenInclude(wag_acts => wag_acts.OutgoingUzVagonActs)
-        //                .Include(wag_doc => wag_doc.OutgoingUzVagons)
-        //                    .ThenInclude(wag_cont => wag_cont.OutgoingUzVagonConts)
-        //                        .ThenInclude(wag_cont_pays => wag_cont_pays.OutgoingUzContPays)
-        //                .Include(wag_doc => wag_doc.OutgoingUzVagons)
-        //                    .ThenInclude(wag_pays => wag_pays.OutgoingUzVagonPays)
-        //                .Include(wag_doc => wag_doc.OutgoingUzVagons)
-        //                    .ThenInclude(wag_cargo => wag_cargo.IdCargoNavigation)
-        //                .Include(wag_doc => wag_doc.OutgoingUzVagons)
-        //                    .ThenInclude(wag_rent => wag_rent.IdWagonsRentArrivalNavigation)
-        //                        .ThenInclude(wag_oper => wag_oper.IdOperatorNavigation)
-        //                .Include(wag_doc => wag_doc.OutgoingUzVagons)
-        //                    .ThenInclude(wag_div => wag_div.IdDivisionOnAmkrNavigation)
-        //                .ToListAsync();
-
-        //        if (result == null)
-        //            return NotFound();
-        //        return new ObjectResult(result);
-        //    }
-        //    catch (Exception e)
-        //    {
-        //        return BadRequest(e.Message);
-        //    }
-        //}
-
-        //// POST: OutgoingUzDocument/update/verification
-        //// BODY: OutgoingUzDocument/update/verification (JSON, XML)
-        //[HttpPost("update/verification")]
-        //public async Task<int> PostVerificationOutgoingUzDocument([FromBody] UpdateVerification value)
-        //{
-        //    try
-        //    {
-        //        string user = HttpContext.User.Identity.Name;
-        //        bool IsAuthenticated = HttpContext.User.Identity.IsAuthenticated;
-        //        // Пользователь непрошел интендификацию!
-        //        if (value == null || !IsAuthenticated) { return -1; }
-        //        if (value.presented < 1 || value.presented > 3) return (int)errors_base.error_input_value;
-        //        List<OutgoingUzDocument>? list = await db.OutgoingUzDocuments
-        //            .Where(d => value.id_docs.Contains(d.Id))
-        //            .ToListAsync();
-        //        if (list != null)
-        //        {
-        //            foreach (OutgoingUzDocument doc in list) {
-
-        //                if (value.presented == 1) {
-        //                    doc.NumActServices1 = value.num_act;
-        //                }
-        //                if (value.presented == 2) {
-        //                    doc.NumActServices2 = value.num_act;
-        //                }
-        //                if (value.presented == 3) {
-        //                    doc.NumActServices3 = value.num_act;
-        //                }
-        //                doc.Verification = DateTime.Now;
-        //                doc.VerificationUser = user;
-        //            }
-        //            return await db.SaveChangesAsync();
-        //        }
-        //        else
-        //        {
-        //            return (int)errors_base.not_inp_uz_doc_db;
-        //        }
-        //    }
-        //    catch (Exception e)
-        //    {
-        //        return (int)errors_base.global;
-        //    }
-        //}
-        //#endregion
+                    }
+                    return await db.SaveChangesAsync();
+                }
+                else
+                {
+                    return (int)errors_base.not_inp_uz_doc_db;
+                }
+            }
+            catch (Exception e)
+            {
+                return (int)errors_base.global;
+            }
+        }
+        #endregion
 
         // POST: OutgoingUzDocument/update/pay
         // BODY: OutgoingUzDocument/update/pay (JSON, XML)
