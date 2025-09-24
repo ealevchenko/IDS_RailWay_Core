a2f09be96e3a96d8df128feb7ca00d884f720bbe
Levchenko <eduard.levchenko_old@arcelormittal.com>
2025-09-24 15:23:06 +0300

    Переделал в функциях выбор письма

---
+++ sql/test_db_update_24_09_2025/тест новых функций.sql
@@ -0,0 +1,14 @@
+USE [KRR-PA-CNT-Railway-Test]
+
+--select * from [IDS].[get_current_operation_wagon_of_id_wir](1132614) -- отмена
+--select * from [IDS].[get_current_operation_wagon_of_id_wir](1132624)
+
+--select * from [IDS].[get_current_operation_wagon_of_num](53146635) -- отмена
+--select * from [IDS].[get_current_operation_wagon_of_num](63524086)
+
+--select * from [IDS].[get_view_incoming_cars_of_id_car](2029653)-- отмена
+--select * from [IDS].[get_view_incoming_cars_of_id_car](2029663)
+--select * from [IDS].[get_view_incoming_cars_of_id_car](2029705)-- не заходил
+
+--select * from [IDS].[get_view_incoming_cars_of_id_sostav](399457)
+
