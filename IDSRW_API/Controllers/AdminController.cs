f52a33f61101134cb16a4397dae995a01d04eb8a
Levchenko, Eduard A <eduard.levchenko@uakrr.com>
2026-07-07 14:52:34 +0300

    Правка админки 1. Добавил проверку с учетом домена 2. Добавил проверку на все роли

--- IDSRW_API/Controllers/AdminController.cs
+++ IDSRW_API/Controllers/AdminController.cs
@@ -17,6 +17,11 @@ namespace IDSRW_API.Controllers
         public string DataBase { get; set; }
         public string NameServer { get; set; }
     }
+    public class RoleInfo
+    {
+        public string Role { get; set; }
+        public bool IsRole { get; set; }
+    }
 
     [Route("[controller]")]
     [ApiController]
@@ -78,12 +83,92 @@ namespace IDSRW_API.Controllers
             {
                 bool isRole = false;
                 bool IsAuthenticated = HttpContext.User.Identity.IsAuthenticated;
+                string[] parts = HttpContext.User.Identity.Name.Split('\\');
+                string domain = parts.Length > 0 ? parts[0] : string.Empty;
                 if (IsAuthenticated)
                 {
-                    isRole = HttpContext.User.IsInRole(role);
+                    if (!String.IsNullOrWhiteSpace(domain))
+                    {
+                        isRole = HttpContext.User.IsInRole(domain + "\\" + role);
+                    }
+                    else
+                    {
+                        isRole = HttpContext.User.IsInRole(role);
+                    }
 
                 }
                 return new ObjectResult(isRole);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        // Admin/get_is_roles/
+        [HttpGet("get_is_roles")]
+        public async Task<ActionResult> GetIsRoles()
+        {
+            try
+            {
+                List<RoleInfo> result = new List<RoleInfo>();
+
+                string[] roles = new string[] {
+                    "KRR-LG_TD-IDSRW_ACCEPT",
+"KRR-LG_TD-IDSRW_SEND",
+"KRR-LG_TD-IDSRW_TRF_ACCEPT",
+"KRR-LG_TD-IDSRW_TRF_SEND",
+"KRR-LG_TD-IDSRW_DOK_ACCEPT",
+"KRR-LG_TD-IDSRW_DOK_SEND",
+"KRR-LG_TD-IDSRO_DOK",
+"KRR-LG_TD-IDSRO_LET_WORK",
+"KRR-LG_TD-IDSRW_ADMIN",
+"KRR-LG_TD-IDSRW_PAY",
+"KRR-LG_TD-IDSRW_LETTERS",
+"KRR-LG_TD-IDSRW_DIRECTORY",
+"KRR-LG_TD-IDSRW_ADDRESS",
+"KRR-LG_TD-IDSRW_COM_STAT",
+"KRR-LG_TD-IDSRW_COND_ARR",
+"KRR-LG_TD-IDSRW_COND_SEND",
+"KRR-LG_TD-IDSRW_ARM_TROP",
+"KRR-LG_TD-IDSRW_ARM_ OPERATIONS",
+"KRR-LG_TD-IDSRW_ARM_OR",
+"KRR-LG_TD-IDSRW_ARM_ NOTE",
+"KRR-LG_TD-IDSRO_ARM",
+"KRR-LG_TD-IDSRW_TIME",
+"KRR-LG_TD-IDSRW_CORREECT",
+"KRR-LG_TD-IDSRO_ACCEPT",
+"KRR-LG_TD-IDSRO_SEND",
+"KRR-LG_TD-IDSRO_HISTORI",
+"KRR-TD-IDSRW_PARK",
+"KRR-LG_TD-IDSRO_ REPORT",
+"KRR-LG_TD-IDSRO_ REPORT_ACCEPT",
+"KRR-LG_TD-IDSRO_ REPORT_SEND",
+"KRR-LG_TD-IDSRO_ REPORT_PLATA",
+"KRR-LG_TD-IDSRO_ REPORT_REMAINDER",
+"KRR-LG_TD-IDSRO_ REPORT_CROSSING"};
+
+                bool IsAuthenticated = HttpContext.User.Identity.IsAuthenticated;
+                string[] parts = HttpContext.User.Identity.Name.Split('\\');
+                string domain = parts.Length > 0 ? parts[0] : string.Empty;
+                if (IsAuthenticated)
+                {
+
+                    foreach (string role in roles)
+                    {
+                        bool isRole = false;
+                        if (!String.IsNullOrWhiteSpace(domain))
+                        {
+                            isRole = HttpContext.User.IsInRole(domain + "\\" + role);
+                        }
+                        else
+                        {
+                            isRole = HttpContext.User.IsInRole(role);
+                        }
+                        result.Add(new RoleInfo { Role = role, IsRole = isRole });
+                    }
+                }
+                return new ObjectResult(result);
             }
             catch (Exception e)
             {
